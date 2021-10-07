ad2415a62c78ebbec63e1f4c123d330a16470971
Ellisson Mateus Resende Moraes <ellissonmrm1@outlook.com>
2021-10-07 19:56:42 -0300

    SellerList

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -82,7 +82,7 @@ public class SellerDaoJDBC implements SellerDao{
 	}
 
 	@Override
-	public void deleteByid(Integer id) {
+	public void deleteById(Integer id) {
 		PreparedStatement st = null;
 		try {
 			st = conn.prepareStatement("delete from seller where Id = ?");
